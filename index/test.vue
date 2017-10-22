b5e478e3056bb96004d0c2bf25af98de186547bc
kengJ <xheiyu@126.com>
2017-10-22 22:47:00 +0800

    update

---
+++ index/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    {{title}}
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'test',
+    data(){
+      return {
+        title:'test123'
+      }
+    }
+  }
+</script>
+
+<style>
+  .test{
+    color: red;
+  }
+</style>
